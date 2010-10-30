afd8c0694d98b49989c4c9d1147774515b90c8f3
Adam Weller <minch@trazzler.com>
2010-10-30 16:22:27 -0400

    Just formatting:  didn't realize I had borked .vimrc tab settings.

--- lib/buoy_data/buoy.rb
+++ lib/buoy_data/buoy.rb
@@ -1,7 +1,7 @@
 module BuoyData
-	class Buoy
-		include HTTParty
+  class Buoy
+    include HTTParty
 
-		GET_SUCCESS = 200
-	end
+    GET_SUCCESS = 200
+  end
 end
--- spec/noaa_buoy_spec.rb
+++ spec/noaa_buoy_spec.rb
@@ -5,16 +5,16 @@ describe BuoyData::NoaaBuoy do
     #WebMock.allow_net_connect! # TODO
     @noaa_buoy = BuoyData::NoaaBuoy.new(41114)
   end
-  
+
   context 'get' do
     it 'should get buoy data' do
-			@noaa_buoy.get
-			@noaa_buoy.response.code.should == BuoyData::NoaaBuoy::GET_SUCCESS
+      @noaa_buoy.get
+      @noaa_buoy.response.code.should == BuoyData::NoaaBuoy::GET_SUCCESS
     end
 
-		it 'should respond to noaa fields' do
-			@noaa_buoy.get
-			@noaa_buoy.should respond_to :WVHT
-		end
-	end
+    it 'should respond to noaa fields' do
+      @noaa_buoy.get
+      @noaa_buoy.should respond_to :WVHT
+    end
+  end
 end
