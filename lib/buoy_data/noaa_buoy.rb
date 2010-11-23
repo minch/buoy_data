760cf3d77b2818719398956e22e4fe9fdedefe6b
Adam Weller <minch@trazzler.com>
2010-11-22 20:39:17 -0500

    Added get_all (for all historical data available via the given url) and google_chart_url method that will get a very simple graph of the last 5 days of historical wave height (WVHT) data for the given station.
    Updated the specs but they are in dire need of
    fakeweb / webmock.

--- lib/buoy_data/noaa_buoy.rb
+++ lib/buoy_data/noaa_buoy.rb
@@ -4,8 +4,9 @@ module BuoyData
 
     attr_accessor :buoy_id, :url, :response
 
-    def initialize(buoy_id)
+    def initialize(buoy_id, unit_system = self.class.default_unit_system)
       @url = base_url(buoy_id)
+      @unit_system = unit_system
     end
 
     def metaclass
@@ -23,19 +24,73 @@ module BuoyData
       parse_response
     end
 
+    def get_all
+      response = get(true)
+      return unless response
+
+      response.parsed_response.split(/\n/)
+    end
+
+    #
+    # Get a graph of the historical data for the given buoy.
+    #
+    # Inspired by:
+    #
+    # https://github.com/thepug/nbdc_graph/blob/master/generate_graphs.py
+    #
+    def google_chart_url
+      max = 120
+      response = get_all
+      return unless response
+
+      historical_data = []
+      response.each_with_index do |row, index|
+        break if index >= max
+        next if row.match(/^#/)
+
+        row = row.split(/  ?/)
+        historical_data << row[5]
+      end
+      return if historical_data.blank?
+      historical_data = historical_data.join(',')
+
+      [ self.class.google_chart_base(@buoy_id), '&chd=t:', historical_data ].join
+    end
+
     def base_url(buoy_id = @buoy_id)
       @buoy_id ||= buoy_id
 
       # The following didn't work as the :description field contains markup we'd have
       # to parse
       #"http://www.ndbc.noaa.gov/data/latest_obs/#{id}.rss"
+      #
+      # Could also consider:
+      #
+      # http://www.ndbc.noaa.gov/data/5day2/41012_5day.txt
+      #
+      # It has more fields but seems to be an hour hehind the below.
 
-      # TODO:  get historical data from:
       "http://www.ndbc.noaa.gov/data/realtime2/#{buoy_id}.spec"
     end
 
     def self.source
       :noaa
+    end
+
+    def self.google_chart_base(buoy_id)
+      url = [ "http://chart.apis.google.com/chart?cht=lc" ]
+      url << "chtt=#{buoy_id}"
+      url << "chts=666666,12"
+      url << "chco=3366FF"
+      url << "chs=250x100"
+      url << "chds=0,10"
+      url << "chl=WVHT".gsub(/ /, '%20')
+      url.join('&')
+    end
+
+
+    def self.default_unit_system
+      :metric
     end
 
     def parse_response
--- spec/noaa_buoy_spec.rb
+++ spec/noaa_buoy_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe BuoyData::NoaaBuoy do
   before(:each) do
-    #WebMock.allow_net_connect! # TODO
+    #FakeWeb.allow_net_connect = false # TODO
     @noaa_buoy = BuoyData::NoaaBuoy.new(41114)
   end
 
@@ -15,6 +15,14 @@ describe BuoyData::NoaaBuoy do
     it 'should respond to noaa fields' do
       @noaa_buoy.get
       @noaa_buoy.should respond_to :WVHT
+    end
+
+    it 'should get all buoy data' do
+      @noaa_buoy.get_all.size.should >= 1
+    end
+
+    it 'should get all buoy data' do
+      @noaa_buoy.google_chart_url.should match(/chart.apis.google.com/)
     end
   end
 end
