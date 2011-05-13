8954de15c268cf10ee27d0f469929d9599e4a67b
Adam Weller <minch@trazzler.com>
2011-05-12 21:21:50 -0400

    NoaaStation scrape was not returning a UTC :source_updated_at timezone.

--- lib/buoy_data/noaa_station.rb
+++ lib/buoy_data/noaa_station.rb
@@ -193,7 +193,7 @@ module BuoyData
           minutes = time[2..3]
           time = [ hour, minutes ].join(':')
           s = [ date, time ].join('T')
-          t = Time.parse s if s
+          t = Time.parse(s).utc if s
         rescue
         end
       end
--- spec/noaa_station_spec.rb
+++ spec/noaa_station_spec.rb
@@ -4,12 +4,28 @@ describe BuoyData::NoaaStation do
   let (:station) { BuoyData::NoaaStation.new }
 
   it "should scrape station" do
-    data = station.scrape station_url
+    url = station_url
+    data = station.scrape url
     data.should_not be_empty
     data.should be_a(Hash)
 
     data[:wtmp].should be_a(Float)
-    data[:source_updated_at].should be_a(Time)
+    source_updated_at = data[:source_updated_at]
+    source_updated_at.should be_a(Time)
+    source_updated_at.zone.should == 'UTC'
+  end
+
+  it "should scrape buoy" do
+    url = buoy_url
+    data = station.scrape url
+    data.should_not be_empty
+    data.should be_a(Hash)
+
+    data[:wtmp].should be_a(Float)
+    source_updated_at = data[:source_updated_at]
+    source_updated_at.should be_a(Time)
+    source_updated_at.zone.should == 'UTC'
+    p source_updated_at
   end
 
   def station_url
